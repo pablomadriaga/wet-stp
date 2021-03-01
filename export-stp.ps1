931b2864496730de0c4fda5af3c7f8d640570970
pablomadriaga <75373441+pablomadriaga@users.noreply.github.com>
2021-02-28 21:53:32 -0300

    v1

---
+++ export-stp.ps1
@@ -0,0 +1,40 @@
+﻿$vc = Read-Host -Prompt "Incregre el Vcenter"
+
+###########################################################################
+########### Completar la lista si tenemos politicas que excluir ###########
+###########################################################################
+$ExcludedPolicies = @("Politica1-ejemplo-excluir","Politica2-ejemplo-excluir")
+
+
+#Conectarse al Vcenter
+try {
+    Write-Host "Conectando con vCenter $vcenter , por favor espere ..." -ForegroundColor Cyan
+    Connect-VIServer $vc -ErrorAction Stop | Out-Null
+}#Fin del Try
+catch {
+    Write-Host "No se puede conectar con el servidor" -ForegroundColor Yellow
+    Break
+}#Fin del Catch
+
+
+
+#Crea carpeta para contener las politicas
+$dir_politicas = "$env:USERPROFILE\politicas2\"
+mkdir $dir_politicas
+
+
+# Obtenemos datos de las politicas
+$StoragePolicies = Get-SpbmStoragePolicy -Server $vc
+		
+# Las recorremos con el FOR
+Foreach($StoragePolicy in $StoragePolicies) {
+		
+	# Nos quedamos solo con su nombre
+	$PolicyName = $StoragePolicy.Name
+
+	# If the policy is not in the excluded list, then export it to FilePath
+	If ($ExcludedPolicies -notcontains $PolicyName) {
+			
+		Export-SpbmStoragePolicy -FilePath $dir_politicas -StoragePolicy $StoragePolicy.Name -ErrorAction SilentlyContinue		
+		}
+}
---
+++ import-stp.ps1
@@ -0,0 +1,37 @@
+﻿$vc = Read-Host -Prompt "Incregre el Vcenter"
+
+
+#Conectarse al Vcenter
+try {
+    Write-Host "Conectando con vCenter $vcenter , por favor espere ..." -ForegroundColor Cyan
+    Connect-VIServer $vc -ErrorAction Stop | Out-Null
+}#Fin del Try
+catch {
+    Write-Host "No se puede conectar con el servidor" -ForegroundColor Yellow
+    Break
+}#Fin del Catch
+
+
+
+# Obtener todos los xml
+$dir_politicas = "$env:USERPROFILE\politicas\"
+$PolicyFiles = Get-ChildItem $dir_politicas -Filter *.xml
+
+#Los pasamos por el for
+Foreach ($PolicyFile in $PolicyFiles) {
+		
+    # Ruta de la politica 
+    $PolicyFilePath = $PolicyFile.FullName
+    
+	# Obtengo el contenido del xml
+	$xml = [xml](Get-Content $PolicyFilePath)
+
+	# Navegamos por el xml y se extrae el nombre
+	$PolicyName = $xml.PbmCapabilityProfile.Name.'#text'
+
+	# Navegamos por el xml y se extrae la descripcion
+	$PolicyDescription = $xml.PbmCapabilityProfile.Description.'#text'
+
+    Write-Host "$PolicyName" -foregroundcolor green
+    Import-SpbmStoragePolicy -Name $PolicyName -Description $PolicyDescription -FilePath $PolicyFilePath # -ErrorAction SilentlyContinue
+}
